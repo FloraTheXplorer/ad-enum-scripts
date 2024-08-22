b07b455b386dd9b5df307996a1d6d84ccf2d5a31
FloraTheExplorer <98337702+FloretteW@users.noreply.github.com>
2024-08-22 12:31:18 +0200

    Update scan_live_hosts.sh

--- scan_live_hosts.sh
+++ scan_live_hosts.sh
@@ -33,17 +33,3 @@ export SCANS
 while read -r TARGET; do
   run_scans "$TARGET" &
 done < "$LIVE_HOSTS_FILE" | xargs -P 5 -I {} sh -c '{}'
-
-# Wait for all background processes to finish
-wait
-
-# Consolidate the results using nmap-parse-output
-while read -r TARGET; do
-  FILENAME="${OUTPUT_DIR}/${TARGET}_nmap_scans.txt"
-  if [ -f "$FILENAME" ]; then
-    nmap-parse-output -o "${OUTPUT_DIR}/${TARGET}_consolidated.txt" "$FILENAME"
-  else
-    echo "File $FILENAME does not exist. Skipping consolidation for $TARGET."
-  fi
-done < "$LIVE_HOSTS_FILE"
-
